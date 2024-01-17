f965b2086d00d7d5032883266851c6c76c5df616
Marcus Hsieh <marcus.j.hsieh@gmail.com>
2024-01-16 17:25:54 -0800

    Commenting

--- AirfoilModifier.m
+++ AirfoilModifier.m
@@ -3,7 +3,7 @@
 classdef AirfoilModifier < handle
     properties
         airfoilCoordinates
-        flipped
+        doFlip
         angleOfAttack
         scale
         chordLength
@@ -12,9 +12,9 @@ classdef AirfoilModifier < handle
     
     methods
         % Constructor
-        function obj = AirfoilModifier(airfoilCoordinates, flipped, angleOfAttack, scale, fileName)
+        function obj = AirfoilModifier(airfoilCoordinates, doFlip, angleOfAttack, scale, fileName)
             obj.airfoilCoordinates = airfoilCoordinates;
-            obj.flipped = flipped;
+            obj.doFlip = doFlip;
             obj.angleOfAttack = angleOfAttack*-1;
             obj.scale = scale;
             obj.fileName = fileName;
@@ -24,30 +24,39 @@ classdef AirfoilModifier < handle
 
         % calculate chord length
         function calcChordLen(obj)
+            % max x coord - min x coord
             obj.chordLength = max(obj.airfoilCoordinates(:, 1)) - min(obj.airfoilCoordinates(:, 1));
         end
 
         % scale airfoil
         function scaleAirfoil(obj)
+            % multiplies all coords by some scale
             obj.airfoilCoordinates = obj.scale * obj.airfoilCoordinates;
         end
         
         % flip airfoil
         function flipAirfoil(obj)
-            if obj.flipped
+            % if doFlip == true
+            if obj.doFlip
+                % multiply all y coord by -1, mirrored vertically
                 obj.airfoilCoordinates(:, 2) = -obj.airfoilCoordinates(:, 2);
             end
         end
         
         % rotate airfoil
         function rotateAirfoil(obj)
+            % convert AOA from deg -> rad
             theta = deg2rad(obj.angleOfAttack);
+            % create 2x2 rotation matrix based on 
+            % sin/cos values calculated from AOA 
             rotationMatrix = [cos(theta), -sin(theta); sin(theta), cos(theta)];
+            % multiply all coords by rotation matrix 
             obj.airfoilCoordinates = obj.airfoilCoordinates * rotationMatrix;
         end
         
         % export modified airfoil to text file
         function exportAirfoil(obj)
+            % saves X Y Z into modifiedAirfoil and exports to some fileName
             modifiedAirfoil = [obj.airfoilCoordinates(:, 1), obj.airfoilCoordinates(:, 2), obj.airfoilCoordinates(:, 3)];
             save(obj.fileName, 'modifiedAirfoil', '-ascii');
         end
@@ -55,6 +64,7 @@ classdef AirfoilModifier < handle
         % plot airfoil
         function plot(obj)
             figure;
+            % only using X Y coords for plot
             plot(obj.airfoilCoordinates(:, 1), obj.airfoilCoordinates(:, 2), '-o');
             axis equal;
             grid on;
